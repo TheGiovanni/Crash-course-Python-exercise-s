b5a705acbcbd33aa86b6cf67dfd5df12d216ea59
Gio cachon <giovannicachon@gmail.com>
2021-12-10 18:47:34 -0600

    ultimo ejercicio de cap 2

---
+++ number_nine.py
@@ -0,0 +1,16 @@
+"""write addition, subtraction, multiplication and division operartions that each result in the
+number 9"""
+
+print (5+4)
+
+a = 3
+b = 3
+multiplication = a * b
+print (multiplication)
+
+print (10-1)
+
+a = 81
+b = 9
+division = a/b
+print(division)
