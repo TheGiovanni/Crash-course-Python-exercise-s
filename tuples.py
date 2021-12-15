70b6811dad4be9ff1057adf24bb069a82edd9a75
Gio cachon <giovannicachon@gmail.com>
2021-12-15 05:49:09 -0600

    capitulo 4

---
+++ tuples.py
@@ -0,0 +1,9 @@
+"""A buffet-style offers only five basic foods. think of five simple foods, and store them in a tuple"""
+foods = ('cerdo', 'pollo', 'res', 'pavo', 'vegetales')
+for food in foods:
+    print(food)
+
+foods_modified = ('carnero', 'gallina', 'res', 'pavo', 'vegetales')
+print ("\nModified foods: ")
+for food in foods_modified:
+    print (food)
